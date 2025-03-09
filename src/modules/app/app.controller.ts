86a36a7dad5a143acc9dacfe1452b29a36daa30d
Ruslan <onishruslan@yandex.ru>
2025-03-09 20:39:36 +0300

    test

--- src/modules/app/app.controller.ts
+++ src/modules/app/app.controller.ts
@@ -1,10 +1,16 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller, Get, UseGuards } from '@nestjs/common'
 import { AppService } from './app.service'
+import { ApiBearerAuth, ApiSecurity } from '@nestjs/swagger'
+import { JwtAuthGuard } from '../auth/guards/auth.guard'
+import { ActiveGuard } from '../auth/guards/active.guard'
+import { PermissionGuard } from '../role-permission/guards/permission.guard'
 
 @Controller()
+@ApiSecurity('bearer')
 export class AppController {
     constructor(private readonly appService: AppService) {}
 
+    @UseGuards(JwtAuthGuard, ActiveGuard)
     @Get('status')
     async status() {
         return await this.appService.status()
